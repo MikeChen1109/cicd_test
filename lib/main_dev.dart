84578027b88d6d9529bfc4f0222cae26694e8851
MIke <mike.chen@joocatech.com>
2022-09-16 16:33:49 +0800

    new branch test

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CI/CD Demo',
+      title: 'CI/CD Demo da da da',
       theme: ThemeData(
         // This is the theme of your application.
         //
